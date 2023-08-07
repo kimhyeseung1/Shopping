8ddc33292ade4471dd4f3a47d9deb2c8639ad2df
kimhyeseung <hg724932@gmail.com>
2023-08-07 16:57:32 +0900

    familyactive 올라갔다 닫히기

--- js/script.js
+++ js/script.js
@@ -17,6 +17,22 @@ window.onload = function () {
   function priceToString(price) {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
+  //하단 패밀리 펼침 기능
+  //목록 열기 버튼
+  const openBt = document.querySelector(".footer-link")
+  //목록 닫기 버튼
+  const closeBt = document.querySelector(".family-close")
+  //보여질 패밀리 목록
+  const family = document.querySelector(".family");
+  //기능처리
+  openBt.addEventListener("click" , function(){
+    family.classList.add("active")
+    this.classList.add("active")
+  });
+  closeBt.addEventListener("click" , function(){
+    family.classList.remove("active");
+    openBt.classList.remove("active");
+  })
   // ===============================================================
   // data.json을 로딩
   const xhttp = new XMLHttpRequest();
